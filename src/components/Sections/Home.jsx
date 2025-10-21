f9e8b9eda16c76efee882c8fb3e601d005765005
B V Vijay <bandikar.v.vijay@gmail.com>
2025-10-21 19:04:15 +0530

    Update resume link in Home component

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => (
 
 <div className="button-group">
 <a
-  href="./B_V_VIJAYA_BHASKAR.pdf"
+  href="./B_V_Vijaya_Bhaskar_Resume.pdf"
   target="_blank"
   rel="noopener noreferrer"
   className="btn"
